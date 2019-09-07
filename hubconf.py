a919fe2dc9456f4c458e2410560cd7c1f039a62d
Nic Dahlquist <nicolas.dahlquist@gmail.com>
2019-09-06 22:26:10 -0700

    Add SHA256 to weights.

--- hubconf.py
+++ hubconf.py
@@ -14,8 +14,8 @@ def style_gan(pretrained=False, *args, **kwargs):
 
     model = style_gan.get_style_gan()
 
-    checkpoint = 'https://github.com/lernapparat/lernapparat/releases/download/v2019-02-01/karras2019stylegan-ffhq' \
-                 '-1024x1024.for_g_all.pt'
+    checkpoint = 'https://github.com/ndahlquist/lernapparat/releases/download/0.0.0/karras2019stylegan-ffhq-1024x1024' \
+                 '.for_g_all-b418e00b3f4f99b879c3e805b8c55e.pt'
     if pretrained:
         state_dict = model_zoo.load_url(checkpoint, map_location='cpu')
         model.load_state_dict(state_dict)
